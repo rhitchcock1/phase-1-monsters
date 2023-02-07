daf2caac25fab29531606b405947162d3d5d0607
Robert Hitchcock <robert.e.hitchcock@gmail.com>
2023-02-07 10:19:58 -0500

    changes

--- js/index.js
+++ js/index.js
@@ -2,25 +2,28 @@ fetch (" http://localhost:3000/monsters/?_limit=50&_page2")
 .then(response => response.json())
 .then(monsters => renderMonsters(monsters))
 
-const monsterForm = document.querySelector("#monster-form")
+const monsterFormHolder = document.querySelector("body > h1")
+const monsterForm = document.createElement("form")
+monsterFormHolder.append(monsterForm)
+
   monsterForm.addEventListener("submit", (e) => {
-    console.log(test)
   e.preventDefault();
-  console.log(test)
+  
   
 })
-//const monsterDiv = document.createElement("div")
+
+
 
 //page loads show 50 monsters
 function renderMonsters(monsters){
     monsters.forEach((monster) => {
-    const monsterBody = document.querySelector("body")  
+    const monsterBody = document.querySelector("#create-monster")
     const monsterCage = document.createElement('div')
     const h2 = document.createElement('h2')
     h2.textContent = monster.name
     monsterCage.append(h2)
     const h3 = document.createElement('h3')
-    h3.textContent = Math.floor(monster.age)
+    h3.textContent ="Age:" +  Math.floor(monster.age)
     monsterCage.append(h3)
     const p = document.createElement('p')
     p.textContent = monster.description
@@ -31,13 +34,7 @@ function renderMonsters(monsters){
     monsterBody.append(monsterCage)
     }
   )
-  
+
 }
 
-// const monsterForm = document.querySelector("#monster-form")
-//   monsterForm.addEventListener("submit", (e) => {
-//     console.log(test)
-//   e.preventDefault();
-//   console.log(test)
-  
-// })
+
