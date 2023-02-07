3ddb1d9ade60c62ede98db897243423b48b1f7ee
Robert Hitchcock <robert.e.hitchcock@gmail.com>
2023-02-07 10:45:15 -0500

    trying to form

--- js/index.js
+++ js/index.js
@@ -2,13 +2,37 @@ fetch (" http://localhost:3000/monsters/?_limit=50&_page2")
 .then(response => response.json())
 .then(monsters => renderMonsters(monsters))
 
-const monsterFormHolder = document.querySelector("body > h1")
+const monsterFormHolder = document.querySelector("#create-monster")
 const monsterForm = document.createElement("form")
-monsterFormHolder.append(monsterForm)
+//monsterFormHolder.classname = 'monster-form'
+const inputName = document.createElement("input")
+inputName.type = "text"
+inputName.name = "name"
+inputName.placeholder = "Monster Name"
+inputName.append(monsterForm)
+const br = document.createElement("br")
+br.append(monsterForm)
+const inputAge = document.createElement("input")
+inputAge.type = "number"
+inputAge.name = "age"
+inputAge.placeholder = "Monster Age"
+inputAge.append(monsterForm)
+br.append(monsterForm)
+const inputDescription = document.createElement("input")
+inputDescription.type = "text"
+inputDescription.name = "description"
+inputAge.placeholder = "Monster Description"
+inputDescription.append(monsterForm)
+br.append(monsterForm)
+const submitBttn = document.createElement("button")
+submitBttn.type = "submit"
+submitBttn.name = "submit"
+submitBttn.value = "Create Monster"
+submitBttn.append(monsterForm)
 
+monsterFormHolder.append(monsterForm)
   monsterForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  
   
 })
 
@@ -17,7 +41,7 @@ monsterFormHolder.append(monsterForm)
 //page loads show 50 monsters
 function renderMonsters(monsters){
     monsters.forEach((monster) => {
-    const monsterBody = document.querySelector("#create-monster")
+    const monsterBody = document.querySelector("#monster-container")
     const monsterCage = document.createElement('div')
     const h2 = document.createElement('h2')
     h2.textContent = monster.name
